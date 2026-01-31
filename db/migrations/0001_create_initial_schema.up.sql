6bf169cc33387d5f68999f279d53f3e4486763b5
noritoshi.omachi <noritoshi.omachi@optim.co.jp>
2026-01-31 22:19:58 +0900

    fix:🐛レビュー対応: マイグレファイルの修正(2)(#6)

--- db/migrations/0001_create_initial_schema.up.sql
+++ db/migrations/0001_create_initial_schema.up.sql
@@ -2,12 +2,12 @@ CREATE TYPE sake_category AS ENUM ('BEER', 'JAPANESE_SAKE', 'WINE', 'WHISKEY', '
 
 CREATE TABLE sakes (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(50) NOT NULL,
     category sake_category NOT NULL,
-    subcategory TEXT,
+    subcategory VARCHAR,
     alcohol_percentage NUMERIC(4, 1) NOT NULL CHECK (alcohol_percentage >= 0 AND alcohol_percentage <= 100),
     volume INTEGER NOT NULL CHECK (volume > 0),
-    origin TEXT NOT NULL,
+    origin VARCHAR(50) NOT NULL,
     price INTEGER NOT NULL CHECK (price >= 0) DEFAULT 0,
     stock INTEGER NOT NULL CHECK (stock >= 0),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -16,7 +16,6 @@ CREATE TABLE sakes (
 
 CREATE INDEX idx_sakes_category ON sakes(category);
 CREATE INDEX idx_sakes_name ON sakes(name);
-CREATE INDEX idx_sakes_origin ON sakes(origin);
 
 COMMENT ON TYPE sake_category IS '大分類(酒のカテゴリを定義するENUM)';
 
