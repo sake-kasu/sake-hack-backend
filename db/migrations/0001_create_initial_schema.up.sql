8bdefe632418f8e581d4d6062e854e5468ce166b
noritoshi.omachi <noritoshi.omachi@optim.co.jp>
2026-02-01 15:01:53 +0900

    fix:🐛テーブル名の修正(#6)

--- db/migrations/0001_create_initial_schema.up.sql
+++ db/migrations/0001_create_initial_schema.up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TYPE sake_category AS ENUM ('BEER', 'JAPANESE_SAKE', 'WINE', 'WHISKEY', 'SHOCHU', 'COCKTAIL', 'OTHER');
 
-CREATE TABLE sake (
+CREATE TABLE sakes (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
     phonetic VARCHAR(100),
@@ -19,24 +19,24 @@ CREATE TABLE sake (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE INDEX idx_sake_category ON sake(category);
-CREATE INDEX idx_sake_name ON sake(name);
+CREATE INDEX idx_sake_category ON sakes(category);
+CREATE INDEX idx_sake_name ON sakes(name);
 
 COMMENT ON TYPE sake_category IS '大分類(酒のカテゴリを定義するENUM)';
 
-COMMENT ON TABLE sake IS '酒の基本情報を管理するテーブル';
+COMMENT ON TABLE sakes IS '酒の基本情報を管理するテーブル';
 
-COMMENT ON COLUMN sake.id IS '酒の一意識別子';
-COMMENT ON COLUMN sake.name IS '酒の商品名';
-COMMENT ON COLUMN sake.phonetic IS 'ふりがな';
-COMMENT ON COLUMN sake.image_id IS '画像ID';
-COMMENT ON COLUMN sake.category IS '大分類(酒のカテゴリ、(例: ビール、日本酒、ワイン))';
-COMMENT ON COLUMN sake.description IS '酒の詳細情報';
-COMMENT ON COLUMN sake.alcohol_percentage IS 'アルコール度数(%)';
-COMMENT ON COLUMN sake.volume_max IS '最大容量(mL)';
-COMMENT ON COLUMN sake.volume_remain IS '残り容量(%)';
-COMMENT ON COLUMN sake.region IS '産地';
-COMMENT ON COLUMN sake.price IS '価格(円)';
-COMMENT ON COLUMN sake.memo IS 'メモ';
-COMMENT ON COLUMN sake.created_at IS '作成日時';
-COMMENT ON COLUMN sake.updated_at IS '更新日時';
+COMMENT ON COLUMN sakes.id IS '酒の一意識別子';
+COMMENT ON COLUMN sakes.name IS '酒の商品名';
+COMMENT ON COLUMN sakes.phonetic IS 'ふりがな';
+COMMENT ON COLUMN sakes.image_id IS '画像ID';
+COMMENT ON COLUMN sakes.category IS '大分類(酒のカテゴリ、(例: ビール、日本酒、ワイン))';
+COMMENT ON COLUMN sakes.description IS '酒の詳細情報';
+COMMENT ON COLUMN sakes.alcohol_percentage IS 'アルコール度数(%)';
+COMMENT ON COLUMN sakes.volume_max IS '最大容量(mL)';
+COMMENT ON COLUMN sakes.volume_remain IS '残り容量(%)';
+COMMENT ON COLUMN sakes.region IS '産地';
+COMMENT ON COLUMN sakes.price IS '価格(円)';
+COMMENT ON COLUMN sakes.memo IS 'メモ';
+COMMENT ON COLUMN sakes.created_at IS '作成日時';
+COMMENT ON COLUMN sakes.updated_at IS '更新日時';
