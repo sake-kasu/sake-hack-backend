9067ae78f0280268ae294244cfdb34cd9936edf9
takahiro.maki <j39g8bxh@gmail.com>
2026-02-12 22:22:16 +0900

    fix:🐛マイグレーションファイルのCOMMENTを修正

--- db/migrations/0002_create_sake_image.up.sql
+++ db/migrations/0002_create_sake_image.up.sql
@@ -9,5 +9,5 @@ CREATE INDEX idx_sake_images_created_at ON sake_images(created_at);
 COMMENT ON TABLE sake_images IS '酒の画像を管理するテーブル';
 
 COMMENT ON COLUMN sake_images.id IS '画像の一意識別子';
-COMMENT ON COLUMN sake_images.image_url IS 'S3に配置される画像のパス';
+COMMENT ON COLUMN sake_images.object_key IS 'S3に配置される画像のパス';
 COMMENT ON COLUMN sake_images.created_at IS '作成日時';
