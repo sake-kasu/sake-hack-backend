01cdd0bb80a928d663d8c981573e40c97d6de9cc
takahiro.maki <j39g8bxh@gmail.com>
2026-02-21 19:39:22 +0900

    update:⚡CORS設定にX-Like-Tokenヘッダーを追加

--- internal/config/cors.go
+++ internal/config/cors.go
@@ -4,7 +4,7 @@ package config
 type CORSConfig struct {
 	AllowedOrigins   []string `env:"CORS_ALLOWED_ORIGINS" envSeparator:"," envDefault:"http://localhost:3000,http://localhost:8080"`
 	AllowedMethods   []string `env:"CORS_ALLOWED_METHODS" envSeparator:"," envDefault:"GET,POST,PUT,DELETE,PATCH,OPTIONS"`
-	AllowedHeaders   []string `env:"CORS_ALLOWED_HEADERS" envSeparator:"," envDefault:"Origin,Content-Type,Accept,Authorization"`
+	AllowedHeaders   []string `env:"CORS_ALLOWED_HEADERS" envSeparator:"," envDefault:"Origin,Content-Type,Accept,Authorization,X-Like-Token"`
 	ExposedHeaders   []string `env:"CORS_EXPOSED_HEADERS" envSeparator:"," envDefault:"Content-Length"`
 	AllowCredentials bool     `env:"CORS_ALLOW_CREDENTIALS" envDefault:"true"`
 	MaxAge           int      `env:"CORS_MAX_AGE" envDefault:"43200"`
