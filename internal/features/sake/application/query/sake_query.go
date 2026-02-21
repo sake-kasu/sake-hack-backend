c9e2c3632126ffae8b95053d8ee477e72c75a967
takahiro.maki <j39g8bxh@gmail.com>
2026-02-21 18:10:50 +0900

    add:✨SakeQueryにマスタデータ取得メソッドのIF定義を追加

--- internal/features/sake/application/query/sake_query.go
+++ internal/features/sake/application/query/sake_query.go
@@ -18,4 +18,7 @@ type ListSakesFilter struct {
 type SakeQuery interface {
 	List(ctx context.Context, filter ListSakesFilter) ([]entity.SakeListItem, entity.Pagination, error)
 	GetDetail(ctx context.Context, id int32) (*entity.SakeDetail, error)
+	ListKinds(ctx context.Context) ([]entity.SakeKind, error)
+	ListBreweries(ctx context.Context, keyword *string, limit int32) ([]entity.Brewery, error)
+	ListDrinkStyles(ctx context.Context) ([]entity.DrinkStyle, error)
 }
